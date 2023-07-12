a3dc1c5f893a5cb859941ab228ee2f6435b7d6b9
GlebLava <60477842+GlebLava@users.noreply.github.com>
2023-07-12 11:30:46 +0200

    transfer

--- VulkanTest/VulkanTest/src/main_rewrite.cpp
+++ VulkanTest/VulkanTest/src/main_rewrite.cpp
@@ -1,6 +1,8 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+#include "utils.h"
+
 #include <iostream>
 #include <vector>
 #include <optional>
---
+++ VulkanTest/VulkanTest/src/utils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+static std::vector<char> readFile(const std::string& filename);
